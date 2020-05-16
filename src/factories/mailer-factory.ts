8a60be73c79f5574495fb7695aef91cb1d8975a7
shabab477 <shababkarim93@gmail.com>
2020-05-16 15:48:32 +0600

    Added mailgun as mailing provider

--- src/factories/mailer-factory.ts
+++ src/factories/mailer-factory.ts
@@ -1,22 +1,12 @@
-import * as nodemailer from 'nodemailer'
+import { NodeMailgun } from 'ts-mailgun';
 
-const key = 'key-2a4b140e1677f3d2d6f10e6f7efddf2f'
-const domain = 'https://app.mailgun.com/app/sending/domains/sandbox4a6ef85b353440139ed2cd8e9b70eab8.mailgun.org'
+const key = 'key-411736ae5be6b1608bdc2f058b7c9509'
+const domain = 'mg.webilizerr.com'
 
-// TODO: This is for mail gun, need to setup DNS for it to work
-// const mailer = new NodeMailgun(key, domain)
-// mailer.fromEmail = 'postmaster@sandbox4a6ef85b353440139ed2cd8e9b70eab8.mailgun.org'
-// mailer.fromTitle = 'Webbilizer'
+const mailer = new NodeMailgun(key, domain)
+mailer.fromEmail = 'mailgun@mg.webilizerr.com'
+mailer.fromTitle = 'Webbilizer'
 
-// mailer.init();
-
-const mailer = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'webbilizertestapp@gmail.com',
-      pass: 'webbilizertestapp123'
-    }
-  });
-
+mailer.init();
 
 export default mailer
