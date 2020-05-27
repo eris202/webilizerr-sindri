7827d52fafcab2e4d2e11c4ad02d4f1464606662
Sindri <55@55.is>
2020-05-27 13:58:34 +0200

    Updated report and stripe UI and front page

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,38 +1,38 @@
-import {ReportController } from '../controllers/reportController'
-import { AuthController} from '../controllers/authController'
-import {Container} from 'typedi'
-import { InvoiceController } from '../controllers/invoiceController';
+import { ReportController } from "../controllers/reportController";
+import { AuthController } from "../controllers/authController";
+import { Container } from "typedi";
+import { InvoiceController } from "../controllers/invoiceController";
 
-const reportController = Container.get(ReportController)
-const authController = Container.get(AuthController)
-const invoiceController = Container.get(InvoiceController)
+const reportController = Container.get(ReportController);
+const authController = Container.get(AuthController);
+const invoiceController = Container.get(InvoiceController);
 
 const alreadyLoggedInMiddleWare = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        return next()
-    }
+  if (!req.isAuthenticated()) {
+    return next();
+  }
 
-    req.flash('message', 'You cannot access the page while logged in.')
-    return res.redirect("/")
-}
+  req.flash("message", "You cannot access the page while logged in.");
+  return res.redirect("/");
+};
 
 const shouldBeLoggedInMiddleWare = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next()
-    }
+  if (req.isAuthenticated()) {
+    return next();
+  }
 
-    const backUrl = `${req.protocol}://${req.get('Host')}${req.originalUrl}`;
-    return res.redirect(`/login?backUrl=${backUrl}`)
-}
+  const backUrl = `${req.protocol}://${req.get("Host")}${req.originalUrl}`;
+  return res.redirect(`/login?backUrl=${backUrl}`);
+};
 
 export interface RouteMapper {
-    [key: string]: RouteDefinition[]
+  [key: string]: RouteDefinition[];
 }
 
 export class RouteDefinition {
-    method: 'post' | 'get' | 'put' | 'delete'
-    handler: (req, res, next?) => void
-    middleWares?: Array<(req, res, next) => void> = [] 
+  method: "post" | "get" | "put" | "delete";
+  handler: (req, res, next?) => void;
+  middleWares?: Array<(req, res, next) => void> = [];
 }
 
 /*
@@ -42,194 +42,214 @@ export class RouteDefinition {
     will automatically be configured with the express router.
 */
 export const routes: RouteMapper[] = [
-    {
-        '/': [
-            {
-                method: 'post',
-                handler: reportController.postReport,
-            },
-            {
-                method: 'get',
-                handler: reportController.viewHomePage,
-            }
-        ]
-    },
-    {
-        '/forgot-password': [
-            {
-                method: 'get',
-                handler: authController.viewForgotPasswordPage
-            },
-            {
-                method: 'post',
-                handler: authController.postForgotPassword
-            }
-        ]
-    },
-    {
-        '/reset-password': [
-            {
-                method: 'get',
-                handler: authController.viewResetPassword
-            },
-            {
-                method: 'post',
-                handler: authController.postResetPassword
-            }
-        ]
-    },
-    {
-        '/auth/verify': [
-            {
-                method: 'get',
-                handler: authController.verifyUserLink,
-            }
-        ]
-    },
-    {
-        '/report/:reportId': [
-            {
-                method: 'get',
-                handler: reportController.renderReportPage
-            }
-        ]
-    },
-    {
-        '/pricing': [
-            {
-                method: 'get',
-                handler: invoiceController.viewPricingPage
-            }
-        ]
-    },
-    {
-        '/checkout': [
-            {
-                method: 'get',
-                handler: invoiceController.viewCheckoutPage,
-                middleWares: [
-                    shouldBeLoggedInMiddleWare
-                ]
-            },
-            {
-                method: 'post',
-                handler: invoiceController.postCheckout,
-                middleWares: [
-                    shouldBeLoggedInMiddleWare
-                ]
-            }
-        ]
-    },
-    {
-        '/logout': [
-            {
-                method: 'get',
-                handler: authController.logout
-            }
-        ]
-    },
-    {
-        '/login': [
-            {
-                method: 'get',
-                handler: authController.viewLoginPage,
-                middleWares: [
-                    alreadyLoggedInMiddleWare
-                ]
-            },
-            {
-                method: 'post',
-                handler: authController.postLogin,
-                middleWares: [
-                    alreadyLoggedInMiddleWare
-                ]
-            }
-        ]
-    },
-    {
-        '/forgotpassword': [
-            {
-                method: 'get',
-                handler: authController.viewForgotPasswordPage,
-                middleWares: [
-                    alreadyLoggedInMiddleWare
-                ]
-            },
-            {
-                method: 'post',
-                handler: authController.postForgotPassword,
-                middleWares: [
-                    alreadyLoggedInMiddleWare
-                ]
-            }
-        ]
-    },
-    {
-        '/register': [
-            {
-                method: 'get',
-                handler: authController.viewRegisterPage,
-                middleWares: [
-                    alreadyLoggedInMiddleWare
-                ]
-            },
-            {
-                method: 'post',
-                handler: authController.postRegister,
-                middleWares: [
-                    alreadyLoggedInMiddleWare
-                ]
-            }
-        ]
-    },
-    {
-        '/features': [
-            {
-                method: 'get',
-                handler: (req, res) => res.render('features')
-            }
-        ]
-    },
-    {
-        '/about-us': [
-            {
-                method: 'get',
-                handler: (req, res) => res.render('about-us')
-            }
-        ]
-    },
-    {
-        '/appointment': [
-            {
-                method: 'get',
-                middleWares: [
-                    shouldBeLoggedInMiddleWare
-                ],
-                handler: (req, res) => res.render('appointment')
-            },
-            {
-                method: 'post',
-                middleWares: [
-                    shouldBeLoggedInMiddleWare
-                ],
-                handler: invoiceController.postAppointment
-            }
-        ]
-    },
-    {
-        '/get-in-touch': [
-            {
-                method: 'get',
-                handler: (req, res) => res.render('get-in-touch')
-            }
-        ]
-    },
-    {
-        '/thank-you': [
-            {
-                method: 'get',
-                handler: (req, res) => res.render('thank-you')
-            }
-        ]
-    },
-]
+  {
+    "/": [
+      {
+        method: "post",
+        handler: reportController.postReport,
+      },
+      {
+        method: "get",
+        handler: reportController.viewHomePage,
+      },
+    ],
+  },
+  {
+    "/forgot-password": [
+      {
+        method: "get",
+        handler: authController.viewForgotPasswordPage,
+      },
+      {
+        method: "post",
+        handler: authController.postForgotPassword,
+      },
+    ],
+  },
+  {
+    "/reset-password": [
+      {
+        method: "get",
+        handler: authController.viewResetPassword,
+      },
+      {
+        method: "post",
+        handler: authController.postResetPassword,
+      },
+    ],
+  },
+  {
+    "/auth/verify": [
+      {
+        method: "get",
+        handler: authController.verifyUserLink,
+      },
+    ],
+  },
+  {
+    "/report/:reportId": [
+      {
+        method: "get",
+        handler: reportController.renderReportPage,
+      },
+    ],
+  },
+  {
+    "/pricing": [
+      {
+        method: "get",
+        handler: invoiceController.viewPricingPage,
+      },
+    ],
+  },
+  {
+    "/checkout": [
+      {
+        method: "get",
+        handler: invoiceController.viewCheckoutPage,
+        middleWares: [shouldBeLoggedInMiddleWare],
+      },
+      {
+        method: "post",
+        handler: invoiceController.postCheckout,
+        middleWares: [shouldBeLoggedInMiddleWare],
+      },
+    ],
+  },
+  {
+    "/logout": [
+      {
+        method: "get",
+        handler: authController.logout,
+      },
+    ],
+  },
+  {
+    "/login": [
+      {
+        method: "get",
+        handler: authController.viewLoginPage,
+        middleWares: [alreadyLoggedInMiddleWare],
+      },
+      {
+        method: "post",
+        handler: authController.postLogin,
+        middleWares: [alreadyLoggedInMiddleWare],
+      },
+    ],
+  },
+  {
+    "/forgotpassword": [
+      {
+        method: "get",
+        handler: authController.viewForgotPasswordPage,
+        middleWares: [alreadyLoggedInMiddleWare],
+      },
+      {
+        method: "post",
+        handler: authController.postForgotPassword,
+        middleWares: [alreadyLoggedInMiddleWare],
+      },
+    ],
+  },
+  {
+    "/register": [
+      {
+        method: "get",
+        handler: authController.viewRegisterPage,
+        middleWares: [alreadyLoggedInMiddleWare],
+      },
+      {
+        method: "post",
+        handler: authController.postRegister,
+        middleWares: [alreadyLoggedInMiddleWare],
+      },
+    ],
+  },
+  {
+    "/features": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("features"),
+      },
+    ],
+  },
+  {
+    "/about-us": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("about-us"),
+      },
+    ],
+  },
+  {
+    "/appointment": [
+      {
+        method: "get",
+        middleWares: [shouldBeLoggedInMiddleWare],
+        handler: (req, res) => res.render("appointment"),
+      },
+      {
+        method: "post",
+        middleWares: [shouldBeLoggedInMiddleWare],
+        handler: invoiceController.postAppointment,
+      },
+    ],
+  },
+  {
+    "/get-in-touch": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("get-in-touch"),
+      },
+    ],
+  },
+  {
+    "/terms": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("terms"),
+      },
+    ],
+  },
+  {
+    "/privacy-policy": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("privacy-policy"),
+      },
+    ],
+  },
+  {
+    "/thank-you": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("thank-you"),
+      },
+    ],
+  },
+  {
+    "/recently-scanned": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("recently-scanned"),
+      },
+    ],
+  },
+  {
+    "/report": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("report"),
+      },
+    ],
+  },
+  {
+    "/pricing-scans": [
+      {
+        method: "get",
+        handler: (req, res) => res.render("pricing-scans"),
+      },
+    ],
+  },
+];
--- src/services/report-service.ts
+++ src/services/report-service.ts
@@ -1,188 +1,203 @@
 import axios from "axios";
 import Report from "../model/Report";
-import seo_API_KEY from '../config/keys'
-import seo_API_URL from '../config/keys'
+import seo_API_KEY from "../config/keys";
+import seo_API_URL from "../config/keys";
 import { Service, Inject } from "typedi";
 import { ReportUiFactory } from "../factories/report-ui-factory";
-import { ReportScoreFactory, ReportScore, ReportGrade } from '../factories/report-score-factory'
-import { CheckSectionUiFactory } from '../factories/check-section-ui-factory'
-import { PerformanceGradeUiFactory } from '../factories/performance-grade-ui-section'
+import {
+  ReportScoreFactory,
+  ReportScore,
+  ReportGrade,
+} from "../factories/report-score-factory";
+import { CheckSectionUiFactory } from "../factories/check-section-ui-factory";
+import { PerformanceGradeUiFactory } from "../factories/performance-grade-ui-section";
 import { HeaderUiFactory } from "../factories/header-section-ui-factory";
 import { MalwareSectionUiFactory } from "../factories/malware-section-ui-factory";
 import { ResponseUiFactory } from "../factories/server-response-ui-factory";
 import { EncodedDataFactory } from "../factories/encoded-data-factory";
 
-
 @Service()
 export class ReportService {
+  //Inject all factory dependencies
+  @Inject() private reportUiFactory: ReportUiFactory;
 
-    //Inject all factory dependencies
-    @Inject() private reportUiFactory: ReportUiFactory
+  @Inject() private reportScoreFactory: ReportScoreFactory;
 
-    @Inject() private reportScoreFactory: ReportScoreFactory
+  @Inject() private checkSectionUiFactory: CheckSectionUiFactory;
 
-    @Inject() private checkSectionUiFactory: CheckSectionUiFactory
+  @Inject() private performanceGradeUiFactory: PerformanceGradeUiFactory;
 
-    @Inject() private performanceGradeUiFactory: PerformanceGradeUiFactory
+  @Inject() private headerUiFactory: HeaderUiFactory;
 
-    @Inject() private headerUiFactory: HeaderUiFactory
+  @Inject() private malwareUiFactory: MalwareSectionUiFactory;
 
-    @Inject() private malwareUiFactory: MalwareSectionUiFactory
+  @Inject() private responseUiFactory: ResponseUiFactory;
 
-    @Inject() private responseUiFactory: ResponseUiFactory
+  @Inject() private encodedDataFactory: EncodedDataFactory;
 
-    @Inject() private encodedDataFactory: EncodedDataFactory
+  renderReportPage = async (
+    reportId
+  ): Promise<{
+    success: any;
+    status: any;
+    data: any;
+    reportId: any;
+  }> => {
+    // Get response from SEO api
+    console.log(seo_API_URL.seoptimerAPI + "get/" + reportId);
+    const response = await axios.get(
+      seo_API_URL.seoptimerAPI + "get/" + reportId,
+      {
+        headers: {
+          "x-api-key": seo_API_KEY.seoptimerKEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    renderReportPage = async (reportId): Promise<{
-        success: any,
-        status: any,
-        data: any,
-        reportId: any
-    }> => {
-
-        // Get response from SEO api
-        console.log(seo_API_URL.seoptimerAPI + "get/" + reportId)
-        const response = await axios.get(seo_API_URL.seoptimerAPI + "get/" + reportId, {
-            headers: {
-                "x-api-key": seo_API_KEY.seoptimerKEY,
-                "Content-Type": "application/json",
-            },
-        })
-
-        // If response is not success then return from here, no need to 
-        // go through the resf of the code
-        if (!response.data.success) {
-            return {
-                status: response.data.success,
-                reportId: reportId,
-                data: false,
-                success: false
-            }
-        }
-
-        // Create a model based on returning data
-        const reportModel = new Report(response.data)
-        const jsonn = reportModel.data.output
-
-        // Score and grade the json data
-        const score = this.calculateScoreFromJson(jsonn)
-        const reportGrade: ReportGrade = score.createGrade()
-
-        // Decorate the json object based on different performance sections
-        this.performanceGradeUiFactory.decoratePerformanceSection(jsonn, reportGrade)
-        this.performanceGradeUiFactory.decorateSecuritySection(jsonn, reportGrade)
-        this.performanceGradeUiFactory.decorateSeoSection(jsonn, reportGrade)
-        this.performanceGradeUiFactory.decorateSocialSection(jsonn, reportGrade)
-        this.performanceGradeUiFactory.decorateOverallSection(jsonn, reportGrade)
-        this.performanceGradeUiFactory.decorateOverallSection(jsonn, reportGrade)
-
-        // Decorate the json object with the calculated grade
-        reportGrade.decorateJsonWithGrades(jsonn)
-
-        // Decorate UI sections of report
-        this.headerUiFactory.decorateHeaderSection(jsonn)
-        this.malwareUiFactory.decorateMalwareSection(jsonn)
-        this.responseUiFactory.decorateResponseUiSection(jsonn)
-        this.encodedDataFactory.decorateEncodedData(jsonn)
-
-        reportModel.data.output = jsonn
-
-        console.log('finish')
-        // Return with success when everything done
-        return {
-            success: reportModel.success,
-            status: reportModel.success,
-            data: reportModel.data,
-            reportId: reportId
-        }
+    // If response is not success then return from here, no need to
+    // go through the resf of the code
+    if (!response.data.success) {
+      return {
+        status: response.data.success,
+        reportId: reportId,
+        data: false,
+        success: false,
+      };
     }
 
-    // Utility function for scoring over the response data
-    private calculateScoreFromJson(json: any) {
-        const score = new ReportScore()
-        console.log(json)
-        const temp: any = json[1];
-        const responseStatus:any = json[0]
-        if (responseStatus) {
-            console.log(temp)
-            // Creating ui sections after api
-            const uiAttributes = this.reportUiFactory.getUiAttributes(temp.passed)
+    // Create a model based on returning data
+    const reportModel = new Report(response.data);
+    const jsonn = reportModel.data.output;
 
-            temp.color = uiAttributes.color
-            temp.warning = uiAttributes.warning
+    // Score and grade the json data
+    const score = this.calculateScoreFromJson(jsonn);
+    const reportGrade: ReportGrade = score.createGrade();
 
-            // Scoring api sums based on performance on sections
-            const reportScore = this.reportScoreFactory.getScore(temp.section, temp.passed)
-            score.sum(reportScore)
-        } else if (responseStatus == "youtubeActivity" || responseStatus == "instagramActivity") {
-            score.sum({
-                socialC: (temp[1]) ? 1 : 0,
-                socialW: (temp[1]) ? 1 : 0
-            })
-        }
+    // Decorate the json object based on different performance sections
+    this.performanceGradeUiFactory.decoratePerformanceSection(
+      jsonn,
+      reportGrade
+    );
+    this.performanceGradeUiFactory.decorateSecuritySection(jsonn, reportGrade);
+    this.performanceGradeUiFactory.decorateSeoSection(jsonn, reportGrade);
+    this.performanceGradeUiFactory.decorateSocialSection(jsonn, reportGrade);
+    this.performanceGradeUiFactory.decorateOverallSection(jsonn, reportGrade);
+    this.performanceGradeUiFactory.decorateOverallSection(jsonn, reportGrade);
 
-        if (responseStatus === "keywords") {
-            if (temp == "passed") {
-                console.log("PASSED");
-            }
+    // Decorate the json object with the calculated grade
+    reportGrade.decorateJsonWithGrades(jsonn);
 
-            const tem = temp['data']['keywords']
+    // Decorate UI sections of report
+    this.headerUiFactory.decorateHeaderSection(jsonn);
+    this.malwareUiFactory.decorateMalwareSection(jsonn);
+    this.responseUiFactory.decorateResponseUiSection(jsonn);
+    this.encodedDataFactory.decorateEncodedData(jsonn);
 
-            for (let check of Object.values(tem)) {
-                this.checkSectionUiFactory.decorateDescriptionSection(check)
-                this.checkSectionUiFactory.decorateTitleSection(check)
-                this.checkSectionUiFactory.decorateHeaderSection(check)
-            }
-        }
+    reportModel.data.output = jsonn;
 
-        return score
+    console.log("finish");
+    // Return with success when everything done
+    return {
+      success: reportModel.success,
+      status: reportModel.success,
+      data: reportModel.data,
+      reportId: reportId,
+    };
+  };
+
+  // Utility function for scoring over the response data
+  private calculateScoreFromJson(json: any) {
+    const score = new ReportScore();
+    console.log(json);
+    const temp: any = json[1];
+    const responseStatus: any = json[0];
+    if (responseStatus) {
+      console.log(temp);
+      // Creating ui sections after api
+      const uiAttributes = this.reportUiFactory.getUiAttributes(temp.passed);
+
+      temp.color = uiAttributes.color;
+      temp.warning = uiAttributes.warning;
+
+      // Scoring api sums based on performance on sections
+      const reportScore = this.reportScoreFactory.getScore(
+        temp.section,
+        temp.passed
+      );
+      score.sum(reportScore);
+    } else if (
+      responseStatus == "youtubeActivity" ||
+      responseStatus == "instagramActivity"
+    ) {
+      score.sum({
+        socialC: temp[1] ? 1 : 0,
+        socialW: temp[1] ? 1 : 0,
+      });
     }
 
-    postApi = async (websiteUrl: String): Promise<ReportApiResponse> => {
-        if (!websiteUrl) {
-            return {
-                data: "",
-                errors: [
-                    "Must enter a website url"
-                ]
-            }
-        }
+    if (responseStatus === "keywords") {
+      if (temp == "passed") {
+        console.log("PASSED");
+      }
 
-        const apiResponse = await axios.post(seo_API_URL + "create",
-            { url: websiteUrl, pdf: 1 },
-            {
-                headers: {
-                    "x-api-key": seo_API_KEY,
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-            })
+      const tem = temp["data"]["keywords"];
 
-        return {
-            data: apiResponse,
-            errors: []
-        }
+      for (let check of Object.values(tem)) {
+        this.checkSectionUiFactory.decorateDescriptionSection(check);
+        this.checkSectionUiFactory.decorateTitleSection(check);
+        this.checkSectionUiFactory.decorateHeaderSection(check);
+      }
     }
 
-    getApiReport = async (reportId: number): Promise<string> => {
-        try {
-            const apiResponse = await axios.get(seo_API_URL + "get/" + reportId, {
-                headers: {
-                    "x-api-key": seo_API_KEY,
-                    "Content-Type": "application/json",
-                },
-            })
+    return score;
+  }
 
-            return apiResponse.data.success ? "ok" : "wait"
-        } catch (e) {
-            console.log(e)
-
-            return e.toString()
-        }
+  postApi = async (websiteUrl: String): Promise<ReportApiResponse> => {
+    if (!websiteUrl) {
+      return {
+        data: "",
+        errors: ["Must enter a website url"],
+      };
     }
+
+    const apiResponse = await axios.post(
+      seo_API_URL + "create",
+      { url: websiteUrl, pdf: 1 },
+      {
+        headers: {
+          "x-api-key": seo_API_KEY,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+
+    return {
+      data: apiResponse,
+      errors: [],
+    };
+  };
+
+  getApiReport = async (reportId: number): Promise<string> => {
+    try {
+      const apiResponse = await axios.get(seo_API_URL + "get/" + reportId, {
+        headers: {
+          "x-api-key": seo_API_KEY,
+          "Content-Type": "application/json",
+        },
+      });
+
+      return apiResponse.data.success ? "ok" : "wait";
+    } catch (e) {
+      console.log(e);
+
+      return e.toString();
+    }
+  };
 }
 
 export interface ReportApiResponse {
-    data: any,
-    errors: string[]
+  data: any;
+  errors: string[];
 }
