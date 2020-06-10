3d60cc9c4582a135efc60dda95e2dd6951bfe5ed
Sindri <55@55.is>
2020-06-10 19:39:15 +0200

    Recently-scanned

--- src/services/report-service.ts
+++ src/services/report-service.ts
@@ -1,18 +1,17 @@
-import axios from "axios"
-import seo_API_KEY from "../config/keys"
-import seo_API_URL from "../config/keys"
-import { Service, Inject } from "typedi"
-import { Output, Data } from '../model/TypedReport'
-import { capitalCase } from 'change-case'
-import blurredKeys from '../config/blur-keys'
-import { ReportCreateResponse } from "../model/ReportCreateResponse"
-import firebaseAdmin from '../config/firebase-setup'
+import axios from "axios";
+import seo_API_KEY from "../config/keys";
+import seo_API_URL from "../config/keys";
+import { Service, Inject } from "typedi";
+import { Output, Data } from "../model/TypedReport";
+import { capitalCase } from "change-case";
+import blurredKeys from "../config/blur-keys";
+import { ReportCreateResponse } from "../model/ReportCreateResponse";
+import firebaseAdmin from "../config/firebase-setup";
 import { ImageService } from "./image-service";
 
 @Service()
 export class ReportService {
-
-  @Inject() private imageService: ImageService
+  @Inject() private imageService: ImageService;
 
   renderReportPage = async (
     reportId
@@ -22,12 +21,11 @@ export class ReportService {
     reportData: any;
     reportId: any;
   }> => {
+    const db = firebaseAdmin.database();
+    const ref = db.ref(`reports/${reportId}`);
+    const snapshot = await ref.once("value");
 
-    const db = firebaseAdmin.database()
-    const ref = db.ref(`reports/${reportId}`)  
-    const snapshot = await ref.once('value') 
-    
-    const typedData = snapshot.val() as Data
+    const typedData = snapshot.val() as Data;
 
     if (!typedData.output.success) {
       return {
@@ -35,12 +33,14 @@ export class ReportService {
         reportData: false,
         reportId: reportId,
         success: false,
-      }
+      };
     }
 
-    const subsections = this.divideResponseToSubsections(typedData.output)
-    const sectionWiseData = this.createSectionWiseData(typedData.output, subsections)
-
+    const subsections = this.divideResponseToSubsections(typedData.output);
+    const sectionWiseData = this.createSectionWiseData(
+      typedData.output,
+      subsections
+    );
 
     // Return with success when everything done
     return {
@@ -54,51 +54,52 @@ export class ReportService {
         screenshot: typedData.output.screenshot,
       },
       reportId: reportId,
-    }
-  }
+    };
+  };
 
   //TODO: Move to a factory
   private createSectionWiseData = (output: Output, subsections) => {
     const seoSection = {
       ...output.scores.seo,
-      ...this.computeSectionScore(subsections['seo']),
-      subSections: subsections['seo'],
-    }
+      ...this.computeSectionScore(subsections["seo"]),
+      subSections: subsections["seo"],
+    };
 
     const uiSection = {
       ...output.scores.ui,
-      ...this.computeSectionScore(subsections['ui']),
-      subSections: subsections['ui'],
-    }
+      ...this.computeSectionScore(subsections["ui"]),
+      subSections: subsections["ui"],
+    };
 
     const performanceSection = {
       ...output.scores.performance,
-      ...this.computeSectionScore(subsections['performance']),
-      subSections: subsections['performance'],
-    }
+      ...this.computeSectionScore(subsections["performance"]),
+      subSections: subsections["performance"],
+    };
 
     const socialSection = {
       ...output.scores.social,
-      ...this.computeSectionScore(subsections['social']),
-      subSections: subsections['social'],
-    }
+      ...this.computeSectionScore(subsections["social"]),
+      subSections: subsections["social"],
+    };
 
     const securitySection = {
       ...output.scores.security,
-      ...this.computeSectionScore(subsections['security']),
-      subSections: subsections['security'],
-    }
+      ...this.computeSectionScore(subsections["security"]),
+      subSections: subsections["security"],
+    };
 
     const technologySection = {
-      ...this.computeSectionScore(subsections['technology']),
-      subSections: subsections['technology'],
-    }
+      ...this.computeSectionScore(subsections["technology"]),
+      subSections: subsections["technology"],
+    };
 
-    const overallSum = seoSection.computedScore
-      + uiSection.computedScore
-      + performanceSection.computedScore
-      + socialSection.computedScore
-      + securitySection.computedScore
+    const overallSum =
+      seoSection.computedScore +
+      uiSection.computedScore +
+      performanceSection.computedScore +
+      socialSection.computedScore +
+      securitySection.computedScore;
 
     return {
       data: {
@@ -110,50 +111,50 @@ export class ReportService {
         technologySection,
       },
       overallSection: {
-        score: Math.round((overallSum * 10) / 50.0)
-      }
-    }
-  }
+        score: Math.round((overallSum * 10) / 50.0),
+      },
+    };
+  };
 
   private computeSectionScore = (subsections: any[]) => {
-    const total = subsections.filter(value => value.passed !== null).length
+    const total = subsections.filter((value) => value.passed !== null).length;
     const passingScore = subsections
-      .filter(value => value.passed !== null && value.passed)
-      .map(value => 1)
-      .reduce((prev, cur) => prev + cur, 0)
+      .filter((value) => value.passed !== null && value.passed)
+      .map((value) => 1)
+      .reduce((prev, cur) => prev + cur, 0);
 
-    const computedScore = Math.round((passingScore * 10) / total)
+    const computedScore = Math.round((passingScore * 10) / total);
 
     return {
       computedScore,
       friendlyComputedScore: `${computedScore}`,
       colorClass: this.getColorClass(computedScore),
-    }
-  }
+    };
+  };
 
   private getColorClass = (score) => {
     if (score < 5) {
-      return 'red'
+      return "red";
     } else if (score >= 5 && score < 8) {
-      return 'orange'
+      return "orange";
     }
 
-    return 'green'
-  }
+    return "green";
+  };
 
   private divideResponseToSubsections = (output: Output) => {
     const subSections = {
-      'seo': [],
-      'ui': [],
-      'performance': [],
-      'social': [],
-      'security': [],
-      'technology': []
-    }
+      seo: [],
+      ui: [],
+      performance: [],
+      social: [],
+      security: [],
+      technology: [],
+    };
 
     for (let [key, value] of Object.entries(output)) {
       if (!value.section) {
-        continue
+        continue;
       }
 
       subSections[value.section].push({
@@ -161,83 +162,99 @@ export class ReportService {
         key: key,
         isBlurred: blurredKeys.indexOf(key) > -1,
         friendlyName: this.changeKeyName(key),
-        passedClass: value.passed ? 'item-num-green' : 'item-num-red',
-        navPassedClass: value.passed ? 'score-item-nav-green' : 'score-item-nav-red',
-        circleTextDisplay: value.passed ? 'P' : 'F'
-      })
+        passedClass: value.passed ? "item-num-green" : "item-num-red",
+        navPassedClass: value.passed
+          ? "score-item-nav-green"
+          : "score-item-nav-red",
+        circleTextDisplay: value.passed ? "P" : "F",
+      });
     }
 
-    return subSections
-  }
+    return subSections;
+  };
 
   private changeKeyName(key: string) {
-    if (key.toLowerCase() === 'analytics') {
-      return "Hit Counts"
+    if (key.toLowerCase() === "analytics") {
+      return "Hit Countsss";
     }
 
-    return capitalCase(key)
+    return capitalCase(key);
   }
 
-  postApi = async (websiteUrl: string): Promise<ReportCreateResponse | {
-    error: string
-  }> => {
+  postApi = async (
+    websiteUrl: string
+  ): Promise<
+    | ReportCreateResponse
+    | {
+        error: string;
+      }
+  > => {
     if (!this.isValidWebsiteUrl(websiteUrl)) {
       return {
-        error: `Invalid url ${websiteUrl}`
-      }
+        error: `Invalid url ${websiteUrl}`,
+      };
     }
 
-    const callbackBasePath = process.env.BASE_HOOK
-    console.log(`${callbackBasePath}/hook`)
+    const callbackBasePath = process.env.BASE_HOOK;
+    console.log(`${callbackBasePath}/hook`);
     console.log({
       url: websiteUrl,
       pdf: 1,
-      callback: `${callbackBasePath}/hook`
-    })
+      callback: `${callbackBasePath}/hook`,
+    });
     console.log({
       "x-api-key": seo_API_KEY.seoptimerKEY,
       "Content-Type": "application/json",
-      "Accept": "application/json",
-    })
+      Accept: "application/json",
+    });
 
     try {
-      return (await axios.post<ReportCreateResponse>(
-        seo_API_URL.seoptimerAPI + "create",
-        {
-          url: websiteUrl,
-          pdf: 1,
-          callback: `${callbackBasePath}/hook`
-        },
-        {
-          headers: {
-            "x-api-key": seo_API_KEY.seoptimerKEY,
-            "Content-Type": "application/json",
-            "Accept": "application/json",
+      return (
+        await axios.post<ReportCreateResponse>(
+          seo_API_URL.seoptimerAPI + "create",
+          {
+            url: websiteUrl,
+            pdf: 1,
+            callback: `${callbackBasePath}/hook`,
           },
-        }
-      )).data
-
+          {
+            headers: {
+              "x-api-key": seo_API_KEY.seoptimerKEY,
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+          }
+        )
+      ).data;
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-
-  }
+  };
 
   saveReport = async (data: Data) => {
-    const database = firebaseAdmin.database()
-    const reference = database.ref(`reports/${data.id}`)
+    const database = firebaseAdmin.database();
+    const reference = database.ref(`reports/${data.id}`);
 
-    data.output.screenshot = await this.imageService.storeImage(data.output.screenshot, data.id)
-    data.output.deviceRendering.data.mobile = await this.imageService.storeImage(data.output.deviceRendering.data.mobile, data.id)
-    data.output.deviceRendering.data.tablet = await this.imageService.storeImage(data.output.deviceRendering.data.tablet, data.id)
+    data.output.screenshot = await this.imageService.storeImage(
+      data.output.screenshot,
+      data.id
+    );
+    data.output.deviceRendering.data.mobile = await this.imageService.storeImage(
+      data.output.deviceRendering.data.mobile,
+      data.id
+    );
+    data.output.deviceRendering.data.tablet = await this.imageService.storeImage(
+      data.output.deviceRendering.data.tablet,
+      data.id
+    );
 
-    await reference.set(JSON.parse(JSON.stringify(data)))
-  }
+    await reference.set(JSON.parse(JSON.stringify(data)));
+  };
 
   private isValidWebsiteUrl(url: string) {
-    const websiteRegex = /^http:\/\/www\.[a-zA-Z\d]+\.[a-zA-Z\d]+$|^https:\/\/www\.[a-zA-Z\d]+\.[a-zA-Z\d]+$/
+    const websiteRegex = /^http:\/\/www\.[a-zA-Z\d]+\.[a-zA-Z\d]+$|^https:\/\/www\.[a-zA-Z\d]+\.[a-zA-Z\d]+$/;
 
-    return websiteRegex.test(url)
+    return websiteRegex.test(url);
   }
 
   getApiReport = async (reportId: number): Promise<string> => {
@@ -247,18 +264,18 @@ export class ReportService {
           "x-api-key": seo_API_KEY,
           "Content-Type": "application/json",
         },
-      })
+      });
 
-      return apiResponse.data.success ? "ok" : "wait"
+      return apiResponse.data.success ? "ok" : "wait";
     } catch (e) {
-      console.log(e)
+      console.log(e);
 
-      return e.toString()
+      return e.toString();
     }
   };
 }
 
 export interface ReportApiResponse {
-  data: any
-  errors: string[]
+  data: any;
+  errors: string[];
 }
