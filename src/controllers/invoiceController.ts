a1ca66042c5aebb6b392bde76dc174d162f0d4df
Sindri <55@55.is>
2020-09-17 17:55:11 +0200

    saving after long rest

--- src/controllers/invoiceController.ts
+++ src/controllers/invoiceController.ts
@@ -1,60 +1,71 @@
-
-import { Service, Inject } from 'typedi'
-import { InvoiceService } from '../services/invoice-service'
-import { ProductPlan } from '../factories/product-plan-factory'
-import User from "../model/User"
-import { MailService } from '../services/mail-service';
+import { Service, Inject } from "typedi";
+import { InvoiceService } from "../services/invoice-service";
+import { ProductPlan } from "../factories/product-plan-factory";
+import User from "../model/User";
+import { MailService } from "../services/mail-service";
 
 @Service()
 export class InvoiceController {
+  @Inject() private invoiceService: InvoiceService;
 
-  @Inject() private invoiceService: InvoiceService
-
-  @Inject() private mailService: MailService
+  @Inject() private mailService: MailService;
 
   viewPricingPage = (req, res) => {
-    res.render('pricing')
-  }
+    res.render("pricing");
+  };
 
   viewCheckoutPage = async (req, res) => {
-    const plan = +req.query.plan
-    const couponId = req.query.coupon
+    const plan = +req.query.plan;
+    const couponId = req.query.coupon;
 
-    const productConfig = ProductPlan.getProductConfig(plan)
+    const productConfig = ProductPlan.getProductConfig(plan);
     const data = {
       price: productConfig.price,
-      discountedPrice: await this.invoiceService.applyDiscount(plan, couponId)
-    }
+      discountedPrice: await this.invoiceService.applyDiscount(plan, couponId),
+    };
 
-    res.render('checkout', data)
-  }
+    res.render("checkout", data);
+  };
 
   postCheckout = async (req, res) => {
-    const email = req.body.email
-    const token = req.body.stripeToken
-    const plan = +req.query.plan
-    const couponName = req.query.coupon
-    
-    const result = await this.invoiceService.subscribeUser(email, token, plan, couponName)
+    const email = req.body.email;
+    const token = req.body.stripeToken;
+    const plan = +req.query.plan;
+    const couponName = req.query.coupon;
+
+    const result = await this.invoiceService.subscribeUser(
+      email,
+      token,
+      plan,
+      couponName
+    );
 
     if (result.error) {
-      console.log('error')
-      return res.redirect(req.originalUrl)
+      console.log("error");
+      return res.redirect(req.originalUrl);
     }
 
-    const productConfig = ProductPlan.getProductConfig(plan)
-    
-    req.flash('message', `You are now using ${productConfig.name}`)
-    
-    return res.redirect(result.redirectUrl)
-  }
+    const productConfig = ProductPlan.getProductConfig(plan);
+
+    req.flash("message", `You are now using ${productConfig.name}`);
+
+    return res.redirect(result.redirectUrl);
+  };
 
   postAppointment = async (req, res) => {
-    const {email, name, url, backendUrl, userName, userPassword, notes} = req.body
-    const localUser = req.user
+    const {
+      email,
+      name,
+      url,
+      backendUrl,
+      userName,
+      userPassword,
+      notes,
+    } = req.body;
+    const localUser = req.user;
 
-    const user = await User.findOne({ email: localUser.email, isActive: true })
-    
+    const user = await User.findOne({ email: localUser.email, isActive: true });
+
     const appointment = {
       email,
       name,
@@ -62,14 +73,14 @@ export class InvoiceController {
       backendUrl,
       userName,
       userPassword,
-      notes
-    }
+      notes,
+    };
 
-    user.appointment = appointment
+    user.appointment = appointment;
 
-    await user.save()
-    this.mailService.sendAppointmentRequest(appointment)
-    
-    return res.redirect('/')
-  }
+    await user.save();
+    this.mailService.sendAppointmentRequest(appointment);
+
+    return res.redirect("/");
+  };
 }
--- src/services/invoice-service.ts
+++ src/services/invoice-service.ts
@@ -2,60 +2,75 @@ import { Service, Inject } from "typedi";
 import { StripeService } from "./stripe-service";
 import User from "../model/User";
 
-import { ProductPlan } from '../factories/product-plan-factory'
+import { ProductPlan } from "../factories/product-plan-factory";
 
 @Service()
 export class InvoiceService {
+  @Inject() private stripeService: StripeService;
 
-    @Inject() private stripeService: StripeService
+  subscribeUser = async (
+    email: String,
+    stripeToken: any,
+    offerPlan: number,
+    couponName: string
+  ) => {
+    const dbUser = await User.findOne({
+      email: email,
+      isActive: true,
+    });
 
-    subscribeUser = async (email: String, stripeToken: any, offerPlan: number, couponName: string) => {
-        const dbUser = await User.findOne({
-            email: email,
-            isActive: true
-        })
-
-        if (!dbUser) {
-            return {
-                error: "The user is inactive or does not exist"
-            }    
-        }
-
-        try {
-            console.log(`here at plan ${offerPlan}`)
-            const config = ProductPlan.getProductConfig(offerPlan)
-
-            if (config.isOneTime) {
-                const paymentIntent = await this.stripeService.createPayment(dbUser.stripeCustomerId, stripeToken, offerPlan, couponName)
-                dbUser.stripeSubscriptionPlanId = paymentIntent.id
-                dbUser.numOfScans += config.scanCount
-                dbUser.productPlan = offerPlan
-            } else {
-                const subscription = await this.stripeService.createSubscription(dbUser.stripeCustomerId, stripeToken, offerPlan, couponName)
-                dbUser.stripeSubscriptionPlanId = subscription.id
-                dbUser.numOfScans += config.scanCount
-                dbUser.productPlan = offerPlan
-            }
-
-            await dbUser.save()
-
-            return {
-                redirectUrl: offerPlan === 3? '/appointment' : '/thank-you'
-            }
-        } catch(e) {
-            console.log(e)
-            return {
-                error: "Please ensure you have a valid coupon by applying it again and check your credentials"
-            }
-        }
+    if (!dbUser) {
+      return {
+        error: "The user is inactive or does not exist",
+      };
     }
 
-    applyDiscount = async (offerPlan: number, coupon: string) => {
-        const config = ProductPlan.getProductConfig(offerPlan)
-        if (!coupon) {
-            return {}
-        }
-        
-        return await this.stripeService.getDiscountPrice(config.price, coupon)
+    try {
+      console.log(`here at plan ${offerPlan}`);
+      const config = ProductPlan.getProductConfig(offerPlan);
+
+      if (config.isOneTime) {
+        const paymentIntent = await this.stripeService.createPayment(
+          dbUser.stripeCustomerId,
+          stripeToken,
+          offerPlan,
+          couponName
+        );
+        dbUser.stripeSubscriptionPlanId = paymentIntent.id;
+        dbUser.numOfScans += config.scanCount;
+        dbUser.productPlan = offerPlan;
+      } else {
+        const subscription = await this.stripeService.createSubscription(
+          dbUser.stripeCustomerId,
+          stripeToken,
+          offerPlan,
+          couponName
+        );
+        dbUser.stripeSubscriptionPlanId = subscription.id;
+        dbUser.numOfScans += config.scanCount;
+        dbUser.productPlan = offerPlan;
+      }
+
+      await dbUser.save();
+
+      return {
+        redirectUrl: offerPlan === 3 ? "/appointment" : "/thank-you",
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        error:
+          "Please ensure you have a valid coupon by applying it again and check your credentials",
+      };
     }
+  };
+
+  applyDiscount = async (offerPlan: number, coupon: string) => {
+    const config = ProductPlan.getProductConfig(offerPlan);
+    if (!coupon) {
+      return {};
+    }
+
+    return await this.stripeService.getDiscountPrice(config.price, coupon);
+  };
 }
